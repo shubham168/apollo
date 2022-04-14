6c634b66f73619ff3204c306eff216157b795c2c
Shubham168 <jadhavshubham168@gmail.com>
2022-04-14 08:34:59 +0530

    First commit

---
+++ src/components/Booking.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Booking = () => {
+  return (
+    <div>Booking
+        <form>
+            <select>
+                <option value="Shekar"></option>
+            </select>
+        </form>
+    </div>
+  )
+}
